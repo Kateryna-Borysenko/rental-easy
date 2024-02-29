9243cdffc11ceba981df0dbb6ad37031de631fa6
Kateryna <k.borysenko.kyiv@gmail.com>
2024-02-29 18:22:55 +0200

    Property and user models and API route and fetch properties

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,11 @@
 import Hero from '@/components/Hero';
 import InfoBoxes from '@/components/ InfoBoxes';
 import HomeProperties from '@/components/HomeProperties';
+
 import connectDB from '@/config/database';
 
 const HomePage = async () => {
-  await connectDB();
+  connectDB;
   return (
     <>
       <Hero />
