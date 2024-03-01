2a5937a5909f2d7ae1cd967ce66de7368885b763
Kateryna <k.borysenko.kyiv@gmail.com>
2024-03-01 13:53:41 +0200

    Google provider setup

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+import AuthProvider from '@/components/AuthProvider';
 import '@/assets/styles/globals.css';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
@@ -10,13 +11,15 @@ export const metadata = {
 
 const MainLayout = ({ children }) => {
   return (
-    <html lang='en'>
-      <body>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang='en'>
+        <body>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
   );
 };
 export default MainLayout;
