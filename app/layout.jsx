d545b4d87fc4ce6ba79e29eecf0dc80398199a55
Kateryna <k.borysenko.kyiv@gmail.com>
2024-03-05 13:35:19 +0200

    Geocoding & Mapbox Map

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import AuthProvider from '@/components/AuthProvider';
 import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import '@/assets/styles/globals.css';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
